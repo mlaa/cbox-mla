a04fd3c745a85fa20b6f5a9edb9086fbc913b4f0
Jonathan Reeve <jon.reeve@gmail.com>
2014-02-20 01:29:48 -0500

    Temporary fix to disable users from editing their full names.

--- members/single/profile/edit.php
+++ members/single/profile/edit.php
@@ -19,13 +19,25 @@ if ( bp_has_profile( 'profile_group_id=' . bp_get_current_profile_group_id() ) )
 
 		<?php while ( bp_profile_fields() ) : bp_the_profile_field(); ?>
 
+
 			<div<?php bp_field_css_class( 'editfield' ); ?>>
 
-				<?php if ( 'textbox' == bp_get_the_profile_field_type() ) : ?>
+				<?php if ( 'textbox' == bp_get_the_profile_field_type() ) :  ?>
 
-				        <h1>Woohoo!</h1> 
-					<label for="<?php bp_the_profile_field_input_name(); ?>"><?php bp_the_profile_field_name(); ?> <?php if ( bp_get_the_profile_field_is_required() ) : ?><?php _e( '(required)', 'buddypress' ); ?><?php endif; ?></label>
-					<input type="text" name="<?php bp_the_profile_field_input_name(); ?>" id="<?php bp_the_profile_field_input_name(); ?>" value="<?php bp_the_profile_field_edit_value(); ?>" <?php if ( bp_get_the_profile_field_is_required() ) : ?>aria-required="true"<?php endif; ?>/>
+					<?php if ( 'Name' == bp_get_the_profile_field_name() ) : ?> <!-- Don't allow users to edit their names. --> 
+
+						<label for="<?php bp_the_profile_field_input_name(); ?>"><?php bp_the_profile_field_name(); ?> <?php if ( bp_get_the_profile_field_is_required() ) : ?><?php _e( '(required)', 'buddypress' ); ?><?php endif; ?></label>
+						<!-- Dummy input, disabled in CSS, because otherwise saving anything causes the user's full name to be overwritten with the username. --> 
+						<input type="text" name="<?php bp_the_profile_field_input_name(); ?>" id="<?php bp_the_profile_field_input_name(); ?>" value="<?php bp_the_profile_field_edit_value(); ?>" <?php if ( bp_get_the_profile_field_is_required() ) : ?>aria-required="true"<?php endif; ?> style="display: none;"/> 
+						<!-- The thing that actually displays. --> 
+						<p id="name"><?php bp_the_profile_field_edit_value();?></p> 
+
+					<?php else : ?> 
+
+						<label for="<?php bp_the_profile_field_input_name(); ?>"><?php bp_the_profile_field_name(); ?> <?php if ( bp_get_the_profile_field_is_required() ) : ?><?php _e( '(required)', 'buddypress' ); ?><?php endif; ?></label>
+						<input type="text" name="<?php bp_the_profile_field_input_name(); ?>" id="<?php bp_the_profile_field_input_name(); ?>" value="<?php bp_the_profile_field_edit_value(); ?>" <?php if ( bp_get_the_profile_field_is_required() ) : ?>aria-required="true"<?php endif; ?>/>
+
+					<?php endif; ?>
 
 				<?php endif; ?>
 
