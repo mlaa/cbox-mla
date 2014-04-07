5338ef134f84553142ead49c3d14672252c79225
Jonathan Reeve <jon.reeve@gmail.com>
2014-04-07 11:26:58 -0400

    updated custom avatar function for new bp version, fixes #1

--- engine/includes/avatars.php
+++ engine/includes/avatars.php
@@ -1,30 +1,30 @@
 <?php
-
 /* Custom blog avatars, 'cause we roll like that!!! */
-
-function custom_avatar ($id, $name) {
-   echo bp_core_fetch_avatar (
-      array(
-         'item_id' => $id,
-         'type' => 'thumb',
-         'alt' => 'Profile picture of site author ' . $name,
-         'width' => 40,
-         'height' => 40,
-         'class' => 'avatar'
-      )
-   );
-}
-
-// From the Executive Director
-function custom_avatar_14 () { custom_avatar (14, 'Rosemary Feal'); }
-add_filter('bp_get_blog_avatar_36', 'custom_avatar_14');
-
-// From the President
-function custom_avatar_205 () { custom_avatar (205, 'Marianne Hirsch'); }
-add_filter('bp_get_blog_avatar_35', 'custom_avatar_205');
-
-// Groups Discussion
-function custom_avatar_153 () { custom_avatar (153, 'Marianne Hirsch'); }
-add_filter('bp_get_blog_avatar_35', 'custom_avatar_153');
-
+function custom_avatar($avatar, $blogid, $params) { 
+	$custom_avatars = array(
+		/* put blog avatars to be overridden here
+		 * in the format: 
+		 * 'blog_id' => array(item_id, 'Person Name'),
+		 */ 
+		'36' => array(14, 'Rosemary Feal'),
+		'35' => array(205, 'Marianne Hirsch'),
+		'153' => array(205, 'Marianne Hirsch'), 
+	); 
+	if (isset($custom_avatars[$blogid])) { 
+		$id = $custom_avatars[$blogid][0]; 
+		$name = $custom_avatars[$blogid][1]; 
+		$avatar = bp_core_fetch_avatar(
+			array(
+				'item_id' => $id,
+				'type' => 'thumb',
+				'alt' => 'Profile picture of site author ' . $name,
+				'width' => 40,
+				'height' => 40,
+				'class' => 'avatar'
+			)
+		); 
+	} 
+	return $avatar; 
+} 
+add_filter('bp_get_blog_avatar', 'custom_avatar', 10, 3);  
 ?>
