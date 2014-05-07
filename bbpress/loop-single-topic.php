9ea999981fced269bd8801702093bbe8f9336db2
Jonathan Reeve <jon.reeve@gmail.com>
2014-05-07 12:30:26 -0400

    remove "topic started by" from forum topic loop

--- bbpress/loop-single-topic.php
+++ bbpress/loop-single-topic.php
@@ -55,12 +55,6 @@
 
 		<p class="bbp-topic-meta">
 
-			<?php do_action( 'bbp_theme_before_topic_started_by' ); ?>
-
-			<span class="bbp-topic-started-by"><?php printf( __( 'Started by: %1$s', 'bbpress' ), bbp_get_topic_author_link( array( 'size' => '14' ) ) ); ?></span>
-
-			<?php do_action( 'bbp_theme_after_topic_started_by' ); ?>
-
 			<?php if ( !bbp_is_single_forum() || ( bbp_get_topic_forum_id() !== bbp_get_forum_id() ) ) : ?>
 
 				<?php do_action( 'bbp_theme_before_topic_started_in' ); ?>
