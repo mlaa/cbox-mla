ed15013ed5495fa4f3d1f5f5c92676c832a58be3
Jonathan Reeve <jon.reeve@gmail.com>
2014-05-22 16:06:58 -0400

    move group filters to engine/includes/custom-filters.php
    this is to unclutter functions.php a little bit

---
+++ engine/includes/custom-filters.php
@@ -0,0 +1,237 @@
+<?php
+/* This script contains Buddypress customizations for MLA group types. */ 
+
+/* This part adds custom MLA filters to the BuddyPress 
+ * Groups directory, allowing users to filter the groups by type
+ * (committees, discussion groups, etc) and by visibility
+ * (private, public, hidden, etc). 
+ */
+// [COMMUNITY STRUCTURE] Add private/public filter to group lists
+
+function mla_group_directory_status_filter() {
+	$str  = '<li class="last filter-status" style="margin-left: 1em; float: left;"><label for="groups-filter-by">Visibility:</label>';
+	$str .= '<select id="groups-filter-by">';
+	$str .= '<option value="all">All</option>';
+	$str .= '<option value="public">Public</option>';
+	$str .= '<option value="private">Private</option>';
+	if (is_admin() || is_super_admin()) {
+		$str .= '<option value="hidden">Hidden</option>';
+	} 
+	$str .= '</select></li>';
+	echo $str;
+}
+function mla_group_directory_type_filter() { 
+	$str  = '<li class="last filter-type" style="margin-left: 1em; float: left;"><label for="groups-filter-by-type">Type:</label>';
+	$str .= '<select id="groups-filter-by-type">';
+	$str .= '<option value="all">All</option>';
+	$str .= '<option value="committees">Committees</option>';
+	$str .= '<option value="divisions">Divisions</option>';
+	$str .= '<option value="discussion_groups">Discussion Groups</option>';
+	$str .= '<option value="other">Other</option>';
+	$str .= '</select></li>';
+	echo $str;
+} 
+add_action( 'bp_groups_directory_group_types', 'mla_group_directory_status_filter');
+add_action( 'bp_groups_directory_group_types', 'mla_group_directory_type_filter');
+
+class BP_Groups_Status_Filter { 
+	protected $status;
+	protected $group_ids = array();
+
+	function __construct( ) {
+
+		$this->status = $_COOKIE['bp-groups-status'];
+		$this->setup_group_ids();
+
+		add_filter( 'bp_groups_get_paged_groups_sql', array( &$this, 'filter_sql' ) );
+		add_filter( 'bp_groups_get_total_groups_sql', array( &$this, 'filter_sql' ) );
+	}
+
+	function setup_group_ids() {
+		global $wpdb, $bp;
+		$sql = $wpdb->prepare( "SELECT id FROM {$bp->groups->table_name} WHERE status = %s", $this->status);
+		$this->group_ids = wp_parse_id_list( $wpdb->get_col( $sql ) );
+	}
+
+	function get_group_ids() {
+		return $this->group_ids;
+	}
+
+	function filter_sql( $sql ) {
+		$group_ids = $this->get_group_ids();
+		if ( empty( $group_ids ) ) {
+			return $sql;
+		}
+
+		$sql_a = explode( 'WHERE', $sql );
+		$new_sql = $sql_a[0] . 'WHERE g.id IN (' . implode( ',', $group_ids ) . ') AND ' . $sql_a[1];
+
+		return $new_sql;
+	}
+
+	function remove_filters() {
+		remove_filter( 'bp_groups_get_paged_groups_sql', array( &$this, 'filter_sql' ) );
+		remove_filter( 'bp_groups_get_total_groups_sql', array( &$this, 'filter_sql' ) );
+	}
+}
+
+class BP_Groups_Type_Filter extends BP_Groups_Status_Filter { 
+
+	protected $status_type;
+	protected $group_ids = array();
+
+	function __construct() { 
+		$this->status_type = $_COOKIE['bp-groups-type']; 
+		$this->setup_group_ids();
+
+		add_filter( 'bp_groups_get_paged_groups_sql', array( &$this, 'filter_sql' ) );
+		add_filter( 'bp_groups_get_total_groups_sql', array( &$this, 'filter_sql' ) );
+	} 
+	function setup_group_ids() {
+		global $wpdb, $bp;
+		$sql_stub = "SELECT group_id FROM {$bp->groups->table_name}_groupmeta WHERE meta_key = 'mla_oid' AND LEFT(meta_value, 1) =  %s"; 
+		switch ( $this->status_type ) { 
+			case "committees": 
+				$sql = $wpdb->prepare($sql_stub, 'M');
+				break; 
+			case "divisions": 
+				$sql = $wpdb->prepare($sql_stub, 'D');
+				break; 
+			case "discussion_groups": 
+				$sql = $wpdb->prepare($sql_stub, 'G');
+				break; 
+			case "other": 
+				$sql = "SELECT DISTINCT group_id from  {$bp->groups->table_name}_groupmeta WHERE group_id NOT IN (SELECT DISTINCT group_id FROM {$bp->groups->table_name}_groupmeta WHERE meta_key = 'mla_oid')";
+				break; 
+		} 
+		$this->group_ids = wp_parse_id_list( $wpdb->get_col( $sql ) );
+	}
+} 
+
+$status_filter = '';
+function add_status_filter() {
+	global $status_filter;
+	if($_COOKIE['bp-group-status']!='all')	$status_filter = new BP_Groups_Status_Filter();
+}
+function remove_status_filter() {
+	global $status_filter;
+	if($_COOKIE['bp-group-status']!='all') $status_filter->remove_filters();
+}
+$type_filter = ''; 
+function add_type_filter() { 
+	global $type_filter; 
+	if($_COOKIE['bp-group-type']!='all') $type_filter = new BP_Groups_Type_Filter();
+} 
+function remove_type_filter() {
+	global $type_filter;
+	if($_COOKIE['bp-group-type']!='all') $type_filter->remove_filters();
+} 
+add_action('bp_before_groups_loop','add_status_filter');
+add_action('bp_after_groups_loop','remove_status_filter');
+add_action('bp_before_groups_loop','add_type_filter');
+add_action('bp_after_groups_loop','remove_type_filter');
+
+function status_filter_js() {
+	if( wp_script_is( 'jquery', 'done' ) ) { ?>
+	<script type="text/javascript">
+	if (jq.cookie('bp-groups-status')) { 
+		jq('li.filter-status select').val(jq.cookie('bp-groups-status'));
+	}
+	jq('li.filter-status select').change( function() {
+
+		if ( jq('.item-list-tabs li.selected').length )
+			var el = jq('.item-list-tabs li.selected');
+		else
+			var el = jq(this);
+
+		var css_id = el.attr('id').split('-');
+		var object = css_id[0];
+		var scope = css_id[1];
+		var status = jq(this).val();
+		var filter = jq('select#groups-order-by').val();
+		var search_terms = '';
+
+		jq.cookie('bp-groups-status',status,{ path: '/' });
+
+		if ( jq('.dir-search input').length )
+			search_terms = jq('.dir-search input').val();
+
+		bp_filter_request( object, filter, scope, 'div.' + object, search_terms, 1, jq.cookie('bp-' + object + '-extras') );
+
+		return false;
+
+	});
+	</script>
+<?php }
+}
+function type_filter_js() {
+	if( wp_script_is( 'jquery', 'done' ) ) { ?>
+	<script type="text/javascript">
+	if (jq.cookie('bp-groups-status')) { 
+		jq('li.filter-type select').val(jq.cookie('bp-groups-type'));
+	} 
+	jq('li.filter-type select').change( function() {
+
+		if ( jq('.item-list-tabs li.selected').length )
+			var el = jq('.item-list-tabs li.selected');
+		else
+			var el = jq(this);
+
+		var css_id = el.attr('id').split('-');
+		var object = css_id[0];
+		var scope = css_id[1];
+		var status = jq(this).val();
+		var filter = jq('select#groups-order-by-type').val();
+		var search_terms = '';
+
+		jq.cookie('bp-groups-type',status,{ path: '/' });
+
+		if ( jq('.dir-search input').length )
+			search_terms = jq('.dir-search input').val();
+
+		bp_filter_request( object, filter, scope, 'div.' + object, search_terms, 1, jq.cookie('bp-' + object + '-extras') );
+
+		return false;
+
+	});
+	</script>
+<?php }
+}
+add_action('wp_footer', 'status_filter_js');
+add_action('wp_footer', 'type_filter_js');
+
+/* This part adds the MLA Group type
+ * (i.e. Committee, Division, Discussion Group)
+ * to the Buddypress Group Type display 
+ */ 
+
+function mla_group_type_filter($type, $group="") { 
+	global $groups_template; 
+
+	if ( empty( $group ) )
+		$group =& $groups_template->group;
+
+	$mla_oid = (groups_get_groupmeta($group->id, 'mla_oid')); 
+	if (!empty($mla_oid)) { 
+		$mla_type_let = $mla_oid[0]; 
+		$type = substr($type, 0, -6); //this avoids redundancy like "Discussion Group (Private Group)" 
+		$visibility = " (".$type.")";
+
+		switch($mla_type_let) { 
+			case "M": 
+				$mla_type = "Committee"; 	
+				break; 
+			case "D": 
+				$mla_type = "Division"; 	
+				break; 
+			case "G": 
+				$mla_type = "Discussion Group"; 	
+				break; 
+		} 
+		$type = $mla_type; 
+		$type .= $visibility; 
+	} 
+	return $type; 	
+} 
+add_filter('bp_get_group_type', 'mla_group_type_filter'); 
+
--- functions.php
+++ functions.php
@@ -1,6 +1,10 @@
 <?php
-//Custom functionality for your CBOX Child Theme.
+
+// Include files instead of cluttering up this file. 
+require_once( 'engine/includes/avatars.php' );
 require_once( 'engine/includes/custom.php' );
+require_once( 'engine/includes/custom-filters.php' );
+
 
 /**
  * Set this to true to put Infinity into developer mode. Developer mode will refresh the dynamic.css on every page load.
@@ -24,236 +28,6 @@ define( 'BPLANG', 'en_US' );
 if ( file_exists( WP_LANG_DIR . '/buddypress-' . BPLANG . '.mo' ) ) {
 	load_textdomain( 'buddypress', WP_LANG_DIR . '/buddypress-' . BPLANG . '.mo' );
 }
-
-
-// [COMMUNITY STRUCTURE] Add private/public filter to group lists
-
-function mla_group_directory_status_filter() {
-	$str  = '<li class="last filter-status" style="margin-left: 1em; float: left;"><label for="groups-filter-by">Visibility:</label>';
-	$str .= '<select id="groups-filter-by">';
-	$str .= '<option value="all">All</option>';
-	$str .= '<option value="public">Public</option>';
-	$str .= '<option value="private">Private</option>';
-	if (is_admin() || is_super_admin()) {
-		$str .= '<option value="hidden">Hidden</option>';
-	} 
-	$str .= '</select></li>';
-	echo $str;
-}
-function mla_group_directory_type_filter() { 
-	$str  = '<li class="last filter-type" style="margin-left: 1em; float: left;"><label for="groups-filter-by-type">Type:</label>';
-	$str .= '<select id="groups-filter-by-type">';
-	$str .= '<option value="all">All</option>';
-	$str .= '<option value="committees">Committees</option>';
-	$str .= '<option value="divisions">Divisions</option>';
-	$str .= '<option value="discussion_groups">Discussion Groups</option>';
-	$str .= '<option value="other">Other</option>';
-	$str .= '</select></li>';
-	echo $str;
-} 
-add_action( 'bp_groups_directory_group_types', 'mla_group_directory_status_filter');
-add_action( 'bp_groups_directory_group_types', 'mla_group_directory_type_filter');
-
-class BP_Groups_Status_Filter { 
-	protected $status;
-	protected $group_ids = array();
-
-	function __construct( ) {
-
-		$this->status = $_COOKIE['bp-groups-status'];
-		$this->setup_group_ids();
-
-		add_filter( 'bp_groups_get_paged_groups_sql', array( &$this, 'filter_sql' ) );
-		add_filter( 'bp_groups_get_total_groups_sql', array( &$this, 'filter_sql' ) );
-	}
-
-	function setup_group_ids() {
-		global $wpdb, $bp;
-		$sql = $wpdb->prepare( "SELECT id FROM {$bp->groups->table_name} WHERE status = %s", $this->status);
-		$this->group_ids = wp_parse_id_list( $wpdb->get_col( $sql ) );
-	}
-
-	function get_group_ids() {
-		return $this->group_ids;
-	}
-
-	function filter_sql( $sql ) {
-		$group_ids = $this->get_group_ids();
-		if ( empty( $group_ids ) ) {
-			return $sql;
-		}
-
-		$sql_a = explode( 'WHERE', $sql );
-		$new_sql = $sql_a[0] . 'WHERE g.id IN (' . implode( ',', $group_ids ) . ') AND ' . $sql_a[1];
-
-		return $new_sql;
-	}
-
-	function remove_filters() {
-		remove_filter( 'bp_groups_get_paged_groups_sql', array( &$this, 'filter_sql' ) );
-		remove_filter( 'bp_groups_get_total_groups_sql', array( &$this, 'filter_sql' ) );
-	}
-}
-
-class BP_Groups_Type_Filter extends BP_Groups_Status_Filter { 
-
-	protected $status_type;
-	protected $group_ids = array();
-
-	function __construct() { 
-		$this->status_type = $_COOKIE['bp-groups-type']; 
-		$this->setup_group_ids();
-
-		add_filter( 'bp_groups_get_paged_groups_sql', array( &$this, 'filter_sql' ) );
-		add_filter( 'bp_groups_get_total_groups_sql', array( &$this, 'filter_sql' ) );
-	} 
-	function setup_group_ids() {
-		global $wpdb, $bp;
-		$sql_stub = "SELECT group_id FROM {$bp->groups->table_name}_groupmeta WHERE meta_key = 'mla_oid' AND LEFT(meta_value, 1) =  %s"; 
-		switch ( $this->status_type ) { 
-			case "committees": 
-				$sql = $wpdb->prepare($sql_stub, 'M');
-				break; 
-			case "divisions": 
-				$sql = $wpdb->prepare($sql_stub, 'D');
-				break; 
-			case "discussion_groups": 
-				$sql = $wpdb->prepare($sql_stub, 'G');
-				break; 
-			case "other": 
-				$sql = "SELECT DISTINCT group_id from  {$bp->groups->table_name}_groupmeta WHERE group_id NOT IN (SELECT DISTINCT group_id FROM {$bp->groups->table_name}_groupmeta WHERE meta_key = 'mla_oid')";
-				break; 
-		} 
-		$this->group_ids = wp_parse_id_list( $wpdb->get_col( $sql ) );
-	}
-} 
-
-$status_filter = '';
-function add_status_filter() {
-	global $status_filter;
-	if($_COOKIE['bp-group-status']!='all')	$status_filter = new BP_Groups_Status_Filter();
-}
-function remove_status_filter() {
-	global $status_filter;
-	if($_COOKIE['bp-group-status']!='all') $status_filter->remove_filters();
-}
-$type_filter = ''; 
-function add_type_filter() { 
-	global $type_filter; 
-	if($_COOKIE['bp-group-type']!='all') $type_filter = new BP_Groups_Type_Filter();
-} 
-function remove_type_filter() {
-	global $type_filter;
-	if($_COOKIE['bp-group-type']!='all') $type_filter->remove_filters();
-} 
-add_action('bp_before_groups_loop','add_status_filter');
-add_action('bp_after_groups_loop','remove_status_filter');
-add_action('bp_before_groups_loop','add_type_filter');
-add_action('bp_after_groups_loop','remove_type_filter');
-
-function status_filter_js() {
-	if( wp_script_is( 'jquery', 'done' ) ) { ?>
-	<script type="text/javascript">
-	if (jq.cookie('bp-groups-status')) { 
-		jq('li.filter-status select').val(jq.cookie('bp-groups-status'));
-	}
-	jq('li.filter-status select').change( function() {
-
-		if ( jq('.item-list-tabs li.selected').length )
-			var el = jq('.item-list-tabs li.selected');
-		else
-			var el = jq(this);
-
-		var css_id = el.attr('id').split('-');
-		var object = css_id[0];
-		var scope = css_id[1];
-		var status = jq(this).val();
-		var filter = jq('select#groups-order-by').val();
-		var search_terms = '';
-
-		jq.cookie('bp-groups-status',status,{ path: '/' });
-
-		if ( jq('.dir-search input').length )
-			search_terms = jq('.dir-search input').val();
-
-		bp_filter_request( object, filter, scope, 'div.' + object, search_terms, 1, jq.cookie('bp-' + object + '-extras') );
-
-		return false;
-
-	});
-	</script>
-<?php }
-}
-function type_filter_js() {
-	if( wp_script_is( 'jquery', 'done' ) ) { ?>
-	<script type="text/javascript">
-	if (jq.cookie('bp-groups-status')) { 
-		jq('li.filter-type select').val(jq.cookie('bp-groups-type'));
-	} 
-	jq('li.filter-type select').change( function() {
-
-		if ( jq('.item-list-tabs li.selected').length )
-			var el = jq('.item-list-tabs li.selected');
-		else
-			var el = jq(this);
-
-		var css_id = el.attr('id').split('-');
-		var object = css_id[0];
-		var scope = css_id[1];
-		var status = jq(this).val();
-		var filter = jq('select#groups-order-by-type').val();
-		var search_terms = '';
-
-		jq.cookie('bp-groups-type',status,{ path: '/' });
-
-		if ( jq('.dir-search input').length )
-			search_terms = jq('.dir-search input').val();
-
-		bp_filter_request( object, filter, scope, 'div.' + object, search_terms, 1, jq.cookie('bp-' + object + '-extras') );
-
-		return false;
-
-	});
-	</script>
-<?php }
-}
-add_action('wp_footer', 'status_filter_js');
-add_action('wp_footer', 'type_filter_js');
-
-/* This part adds the MLA Group type
- * (i.e. Committee, Division, Discussion Group)
- * to the Buddypress Group Type display 
- */ 
-
-function mla_group_type_filter($type, $group="") { 
-	global $groups_template; 
-
-	if ( empty( $group ) )
-		$group =& $groups_template->group;
-
-	$mla_oid = (groups_get_groupmeta($group->id, 'mla_oid')); 
-	if (!empty($mla_oid)) { 
-		$mla_type_let = $mla_oid[0]; 
-		$type = substr($type, 0, -6); //this avoids redundancy like "Discussion Group (Private Group)" 
-		$visibility = " (".$type.")";
-
-		switch($mla_type_let) { 
-			case "M": 
-				$mla_type = "Committee"; 	
-				break; 
-			case "D": 
-				$mla_type = "Division"; 	
-				break; 
-			case "G": 
-				$mla_type = "Discussion Group"; 	
-				break; 
-		} 
-		$type = $mla_type; 
-		$type .= $visibility; 
-	} 
-	return $type; 	
-} 
-add_filter('bp_get_group_type', 'mla_group_type_filter'); 
 
 /* This function filters out membership activities from the group activity stream, 
  * so that "so-and-so joined the group X" doesn't clutter the activity stream. 
@@ -393,14 +167,12 @@ function mla_allowed_tags() {
 
 add_filter('bbp_kses_allowed_tags','mla_allowed_tags');
 
-
 // Adds BBPress "Forums" select option to Advanced Search. - JR
 function mla_bp_search_form_type_select_add_forums($options) { 
 	$options['bbpforums']  = __( 'Forums',  'buddypress' ); 
 	return $options; 
 } 
 add_filter('bp_search_form_type_select_options', 'mla_bp_search_form_type_select_add_forums'); 
-
 
 // Fix forum search handling - JR
 function mla_bp_core_action_search_site( $slug = '') { 
@@ -472,7 +244,6 @@ add_action('bp_init', 'mla_bp_core_action_search_site', 7);
 /*
  * Remove misbehaving forums tab on profile pages.
  */
-
 function remove_forums_nav() {
 	bp_core_remove_nav_item('forums');
 }
@@ -490,7 +261,6 @@ add_action( 'admin_bar_menu', 'mlac_remove_forums_from_adminbar', 9999 );
  * Hide settings page (we don't want users changing their 
  * e-mail or password).
  */
-
 function change_settings_subnav() {
 
 	$args = array(
@@ -502,14 +272,12 @@ function change_settings_subnav() {
 	bp_core_new_nav_default($args);
 
 }
-
 add_action('bp_setup_nav', 'change_settings_subnav', 5);
 
 function remove_general_subnav() {
 	global $bp;
 	bp_core_remove_subnav_item($bp->settings->slug, 'general');
 }
-
 add_action( 'wp', 'remove_general_subnav', 2 );
 
 /* 
