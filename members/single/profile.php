236f95696429273f608ccc5f69d1f3c9428f14a8
Jonathan Reeve <jon.reeve@gmail.com>
2014-06-18 09:14:10 -0400

    clean up profile view

---
+++ members/single/profile.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * BuddyPress - Users Profile
+ *
+ * @package BuddyPress
+ * @subpackage bp-default
+ */
+
+?>
+
+<?php bp_get_options_nav(); ?>
+
+<?php do_action( 'bp_before_profile_content' ); ?>
+
+<div class="profile" role="main">
+
+	<?php
+		// Profile Edit
+		if ( bp_is_current_action( 'edit' ) )
+			locate_template( array( 'members/single/profile/edit.php' ), true );
+
+		// Change Avatar
+		elseif ( bp_is_current_action( 'change-avatar' ) )
+			locate_template( array( 'members/single/profile/change-avatar.php' ), true );
+
+		// Display XProfile
+		elseif ( bp_is_active( 'xprofile' ) )
+			locate_template( array( 'members/single/profile/profile-loop.php' ), true );
+
+		// Display WordPress profile (fallback)
+		else
+			locate_template( array( 'members/single/profile/profile-wp.php' ), true );
+	?>
+
+</div><!-- .profile -->
+
+<?php do_action( 'bp_after_profile_content' ); ?>
