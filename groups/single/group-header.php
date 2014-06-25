1bf7db82d819aa2b8abc1512fe05a109e812aef2
Jonathan Reeve <jon.reeve@gmail.com>
2014-06-25 14:36:18 -0400

    style profile nav in mobile

--- groups/single/group-header.php
+++ groups/single/group-header.php
@@ -54,3 +54,16 @@ do_action( 'bp_before_group_header' );
 do_action( 'bp_after_group_header' );
 do_action( 'template_notices' );
 ?>
+
+<div id="item-nav">
+	<div class="item-list-tabs no-ajax" id="object-nav" role="navigation">
+		<ul>
+
+			<?php bp_get_options_nav(); ?>
+
+			<?php do_action( 'bp_group_options_nav' ); ?>
+
+		</ul>
+	</div>
+</div><!-- #item-nav -->
+
