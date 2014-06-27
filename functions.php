1dcb598904ce7201651571bc9752dddb5aaa9a34
Jonathan Reeve <jon.reeve@gmail.com>
2014-06-27 13:49:01 -0400

    whitespace changes to appease error checker

--- functions.php
+++ functions.php
@@ -1,26 +1,30 @@
 <?php
+/**
+ * This file contains customizations specific to the MLA
+ * implementation of CBOX. 
+ */ 
 
 // Include files instead of cluttering up this file. 
-require_once( 'engine/includes/advanced-search.php' ); 
-require_once( 'engine/includes/allowed-tags.php' ); 
-require_once( 'engine/includes/avatars.php' );
-require_once( 'engine/includes/custom.php' );
-require_once( 'engine/includes/custom-filters.php' );
+require_once 'engine/includes/advanced-search.php'; 
+require_once 'engine/includes/allowed-tags.php'; 
+require_once 'engine/includes/avatars.php';
+require_once 'engine/includes/custom.php';
+require_once 'engine/includes/custom-filters.php';
 
 /**
  * Set this to true to put Infinity into developer mode. Developer mode will refresh the dynamic.css on every page load.
  */
-define( 'INFINITY_DEV_MODE', true );
+define('INFINITY_DEV_MODE', true);
 
 /* This script contains Buddypress customizations for MLA group types. */ 
 
 /* MLA edits to BP literals */
-define ( 'BP_FRIENDS_SLUG', 'contacts' );
+define('BP_FRIENDS_SLUG', 'contacts');
 
 // Change "en_US" to your locale
-define( 'BPLANG', 'en_US' );
-if ( file_exists( WP_LANG_DIR . '/buddypress-' . BPLANG . '.mo' ) ) {
-	load_textdomain( 'buddypress', WP_LANG_DIR . '/buddypress-' . BPLANG . '.mo' );
+define('BPLANG', 'en_US');
+if (file_exists(WP_LANG_DIR . '/buddypress-' . BPLANG . '.mo')) {
+	load_textdomain('buddypress', WP_LANG_DIR . '/buddypress-' . BPLANG . '.mo');
 }
 
 /* This function filters out membership activities from the group activity stream, 
@@ -32,7 +36,7 @@ if ( file_exists( WP_LANG_DIR . '/buddypress-' . BPLANG . '.mo' ) ) {
  * by default. 
  */ 
 function mla_check_create_forum_for_new_group() {
-	if( wp_script_is( 'jquery', 'done' ) ) { ?>
+	if(wp_script_is('jquery', 'done')) { ?>
 		<script type="text/javascript">
 		jq('#bbp-create-group-forum').prop('checked', true);
 		</script>
@@ -41,13 +45,13 @@ function mla_check_create_forum_for_new_group() {
 add_action('wp_footer', 'mla_check_create_forum_for_new_group');
 
 /* disable visual editor entirely, for everyone */ 
-/* add_filter( 'user_can_richedit' , '__return_false', 50 ); */ 
+/* add_filter('user_can_richedit' , '__return_false', 50); */ 
 
 /* 
  * Remove redundant email status button in group headings; 
  * this is handled by the group tab "Email Options" 
  */
-remove_action ( 'bp_group_header_meta', 'ass_group_subscribe_button' );
+remove_action('bp_group_header_meta', 'ass_group_subscribe_button');
 
 /* 
  * Remove forum subscribe link. Users are already subscribed to the forums 
@@ -57,35 +61,35 @@ remove_action ( 'bp_group_header_meta', 'ass_group_subscribe_button' );
 function mla_remove_forum_subscribe_link($link){ 
 	return ""; //making this empty so that it will get rid of the forum subscribe link
 } 
-add_filter( 'bbp_get_forum_subscribe_link', 'mla_remove_forum_subscribe_link');
+add_filter('bbp_get_forum_subscribe_link', 'mla_remove_forum_subscribe_link');
 
 /* 
  * Remove subscription link from groups directory. 
  * Because we're about to rewrite it!
  * Get ready for the magic.  
  */ 
-remove_action ( 'bp_directory_groups_actions', 'ass_group_subscribe_button' );
+remove_action('bp_directory_groups_actions', 'ass_group_subscribe_button');
 
 function mla_ass_group_subscribe_button() {
 	global $bp, $groups_template;
 
-	if( ! empty( $groups_template ) ) {
+	if(! empty( $groups_template)) {
 		$group =& $groups_template->group;
 	}
 	else {
 		$group = groups_get_current_group();
 	}
 
-	if ( !is_user_logged_in() || !empty( $group->is_banned ) || !$group->is_member )
+	if (!is_user_logged_in() || !empty($group->is_banned) || !$group->is_member)
 		return;
 
 	// if we're looking at someone elses list of groups hide the subscription
-	if ( bp_displayed_user_id() && ( bp_loggedin_user_id() != bp_displayed_user_id() ) )
+	if (bp_displayed_user_id() && (bp_loggedin_user_id() != bp_displayed_user_id()))
 		return;
 
-	$group_status = ass_get_group_subscription_status( bp_loggedin_user_id(), $group->id );
+	$group_status = ass_get_group_subscription_status(bp_loggedin_user_id(), $group->id);
 
-	if ( $group_status == 'no' )
+	if ($group_status == 'no')
 		$group_status = NULL;
 
 	$status_desc = __('Your email status is ', 'bp-ass');
@@ -93,13 +97,13 @@ function mla_ass_group_subscribe_button() {
 	$gemail_icon_class = ' gemail_icon';
 	$sep = '';
 
-	if ( !$group_status ) {
+	if (!$group_status) {
 		//$status_desc = '';
 		$link_text = __('Get email updates', 'bp-ass');
 		$sep = '';
 	}
 
-	$status = ass_subscribe_translate( $group_status );
+	$status = ass_subscribe_translate($group_status);
 
 	$notifications_url = home_url().'/groups/'.groups_get_slug($group->id).'/notifications/'; 
 	?>
@@ -111,39 +115,47 @@ function mla_ass_group_subscribe_button() {
 	<?php
 }
 
-add_action ( 'bp_directory_groups_actions', 'mla_ass_group_subscribe_button' );
+add_action('bp_directory_groups_actions', 'mla_ass_group_subscribe_button');
 
 /* Remove forum title, since in our use cases forum titles have the same names as
  * their parent groups, and users see a redundant title on group forums pages. 
  */
 function mla_remove_forum_title($title) { 
 } 
-add_filter( 'bbp_get_forum_title', 'mla_remove_forum_title' ); 
+add_filter('bbp_get_forum_title', 'mla_remove_forum_title'); 
 
 /* 
  * Remove profile group tab from edit profile page when there's only one profile
  * group. I just says "Profile" and is kind of confusing. 
  */ 
 function mla_remove_profile_group_tab($tabs, $groups) { 
-	if ( count( $groups ) > 1 ) { 
+	if (count($groups) > 1) { 
 		return $tabs; 
 	} else { 
 		return; 
 	} 
 } 
-add_filter( 'xprofile_filter_profile_group_tabs', 'mla_remove_profile_group_tab' ); 
+add_filter('xprofile_filter_profile_group_tabs', 'mla_remove_profile_group_tab'); 
+
+/** 
+ * Remove "announcements" tab. 
+ */ 
+function mla_remove_announcements_tab() {
+	bp_core_remove_subnav_item(bp_get_current_group_slug(), 'send-invites');
+} 
+add_action('wp_footer', 'mla_remove_announcements_tab'); 
 
 //debugging
-if(!function_exists('_log')){
-	function _log( $message ) {
-		if( WP_DEBUG === true ){
-			if( is_array( $message ) || is_object( $message ) ){
-				error_log( print_r( $message, true ) );
+if (!function_exists('_log')) {
+	function _log($message) {
+		if (WP_DEBUG === true) {
+			if(is_array($message) || is_object($message)){
+				error_log(print_r($message, true));
 			} else {
-				error_log( $message );
+				error_log($message);
 			}
 		}
 	}
 }
-_log("Bp_nav is: "); 
-_log($bp); 
+//_log("Helloooo!"); 
+//_log($bp->bp_nav); 
