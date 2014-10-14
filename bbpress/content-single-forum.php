7282cdc8739654e7685482b1a618ced7ca85573e
Jonathan Reeve <jon.reeve@gmail.com>
2014-10-14 13:35:35 -0400

    no all caps

--- bbpress/content-single-forum.php
+++ bbpress/content-single-forum.php
@@ -10,7 +10,7 @@
 ?>
 
 	<form role="search" method="get" id="bbp-searchform" action="">
-		<label for="ts">Search this group's forum topics</label>
+		<label for="ts">Search this group's forum topics:</label>
 		<input type="text" name="ts" id="ts" value="<?php echo ( isset( $_REQUEST['ts'] ) ) ? $_REQUEST['ts'] : ''; ?>" />
 		<input type="submit" id="searchsubmit" value="Search" />
 	</form> 
