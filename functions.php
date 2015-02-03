617c029e8c00f13643ff377504a3046503c28c49
Jonathan Reeve <jon.reeve@gmail.com>
2015-02-03 12:52:59 -0500

    filter gettext to translate "Username" -> "User name"
    per mlaa/cbox-auth#3

--- functions.php
+++ functions.php
@@ -483,3 +483,24 @@ function mla_set_default_email_subscription_level( $level ) {
 } 
 add_filter( 'ass_default_subscription_level', 'mla_set_default_email_subscription_level', 99 );
 add_filter( 'ass_get_default_subscription', 'mla_set_default_email_subscription_level', 99 );
+
+function mla_filter_gettext( $translated, $original, $domain ) {
+	// This is an array of original strings
+	// and what they should be replaced with
+	$strings = array(
+		'Username' => 'User name', // per MLA house style
+		// Add some more strings here
+	);
+
+	// See if the current string is in the $strings array
+	// If so, replace it's translation
+	if ( ! empty( $strings[ $original ] ) ) {
+		// This accomplishes the same thing as __()
+		// but without running it through the filter again
+		$translations = &get_translations_for_domain( $domain );
+		$translated = $translations->translate( $strings[ $original ] );
+	}
+
+	return $translated;
+}
+add_filter( 'gettext', 'mla_filter_gettext', 10, 3 ); 
