f2342393dd4a285efb18d9c42352055608cc80f4
Jonathan Reeve <jon.reeve@gmail.com>
2015-05-20 10:58:49 -0400

    initial import from craftpeak; rebranded

---
+++ lib/custom.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MLA\Tuileries\Custom;
+
+/**
+ * Custom functions for the theme should go here. Use namespacing like:
+ *
+ * use MLA\Tuileries\Custom; (at the top of the file)
+ *
+ * Custom\function_name(); (in your PHP code)
+ */
---
+++ templates/page-header.php
@@ -0,0 +1,7 @@
+<?php use MLA\Tuileries\Titles; ?>
+
+<div class="page-header">
+	<h1>
+		<?php echo Titles\title(); ?>
+	</h1>
+</div>
