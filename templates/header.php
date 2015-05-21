571ee7171d58e23f110fae49cfcd7cbc96da4794
Jonathan Reeve <jon.reeve@gmail.com>
2015-05-21 13:21:30 -0400

    flexbox is the greatest thing ever

--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,4 @@
-<header class="banner" role="banner">
+<header id="main-site-header" class="banner" role="banner">
 	<a class="brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">
 		<?php bloginfo( 'name' ); ?>
 	</a>
