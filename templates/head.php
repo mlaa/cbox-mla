d8e82ee6ed22a1f515770d56c01e998c4f1120b3
Jonathan Reeve <jon.reeve@gmail.com>
2015-05-21 15:10:40 -0400

    import typekit, set proxima nova as main font

--- templates/head.php
+++ templates/head.php
@@ -2,5 +2,11 @@
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- TypeKit Embed Code -->
+		<script type="text/javascript" src="//use.typekit.net/wed8osd.js"></script>
+		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+	<!-- End TypeKit Embed Code -->
+
 	<?php wp_head(); ?>
 </head>
