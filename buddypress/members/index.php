d696b6f19d481a7f754c292732d48de5c612dff9
Jonathan Reeve <jon.reeve@gmail.com>
2015-06-05 13:56:01 -0400

    secondary nav for members dir

--- buddypress/members/index.php
+++ buddypress/members/index.php
@@ -2,7 +2,6 @@
 
 <div id="buddypress">
 
-
 	<?php do_action( 'bp_before_directory_members' ); ?>
 
 	<?php do_action( 'bp_before_directory_members_content' ); ?>
@@ -11,46 +10,23 @@
 
 	<form action="" method="post" id="members-directory-form" class="dir-form">
 
-		<section id="secondary-nav">
-
-			<section class="row">
-
+			<!-- Because there are only two tabs here at the moment, we can wrap
+			   - this whole thing in an `if` statement, since single tabs are of
+			   - little use. -->
 			<?php if ( is_user_logged_in() && bp_is_active( 'friends' ) && bp_get_total_friend_count( bp_loggedin_user_id() ) ) : ?>
-				<div class="item-list-tabs" role="navigation">
-					<ul>
-						<li class="selected" id="members-all"><a href="<?php bp_members_directory_permalink(); ?>"><?php printf( __( 'All Members <span>%s</span>', 'buddypress' ), bp_get_total_member_count() ); ?></a></li>
+				<nav class="secondary">
+					<div class="item-list-tabs" role="navigation">
+						<ul>
+							<li class="selected" id="members-all"><a href="<?php bp_members_directory_permalink(); ?>"><?php printf( __( 'All Members <span>%s</span>', 'buddypress' ), bp_get_total_member_count() ); ?></a></li>
 
 							<li id="members-personal"><a href="<?php echo bp_loggedin_user_domain() . bp_get_friends_slug() . '/my-friends/'; ?>"><?php printf( __( 'My Friends <span>%s</span>', 'buddypress' ), bp_get_total_friend_count( bp_loggedin_user_id() ) ); ?></a></li>
 
-						<?php do_action( 'bp_members_directory_member_types' ); ?>
+							<?php do_action( 'bp_members_directory_member_types' ); ?>
 
-					</ul>
-				</div><!-- .item-list-tabs -->
+						</ul>
+					</div><!-- .item-list-tabs -->
+				</nav>
 			<?php endif; ?>
-
-			<div class="item-list-tabs" id="subnav" role="navigation">
-				<ul>
-					<?php do_action( 'bp_members_directory_member_sub_types' ); ?>
-
-					<li id="members-order-select" class="last filter">
-						<label for="members-order-by"><?php _e( 'Order By:', 'buddypress' ); ?></label>
-						<select id="members-order-by">
-							<option value="active"><?php _e( 'Last Active', 'buddypress' ); ?></option>
-							<option value="newest"><?php _e( 'Newest Registered', 'buddypress' ); ?></option>
-
-							<?php if ( bp_is_active( 'xprofile' ) ) : ?>
-								<option value="alphabetical"><?php _e( 'Alphabetical', 'buddypress' ); ?></option>
-							<?php endif; ?>
-
-							<?php do_action( 'bp_members_directory_order_options' ); ?>
-						</select>
-					</li>
-				</ul>
-			</div>
-
-			</section>
-
-		</section><!-- #secondary-nav -->
 
 		<div id="members-dir-list" class="members dir-list">
 			<?php bp_get_template_part( 'members/members-loop' ); ?>
