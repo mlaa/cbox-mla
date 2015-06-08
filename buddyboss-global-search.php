8bfad62f6fce402ab085542ae39421e53f662524
Jonathan Reeve <jon.reeve@gmail.com>
2015-06-08 13:22:50 -0400

    template for search results page

---
+++ buddyboss-global-search.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html class="no-js" <?php language_attributes(); ?>>
+	<?php get_template_part( 'templates/head' ); ?>
+	<body <?php body_class(); ?>>
+		<!--[if lt IE 9]>
+			<div class="alert">
+				<?php _e( 'You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'cpwpst' ); ?>
+			</div>
+		<![endif]-->
+		<?php
+			do_action( 'get_header' );
+			get_template_part( 'templates/header' );
+		?>
+		<div class="wrap" role="document">
+			<div class="content">
+				<main class="main" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'templates/page', 'header' ); ?>
+					<?php get_template_part( 'templates/content', 'page' ); ?>
+				<?php endwhile; ?>
+
+				</main><!-- /.main -->
+			</div><!-- /.content -->
+		</div><!-- /.wrap -->
+		<?php
+			get_template_part( 'templates/footer' );
+			wp_footer();
+		?>
+	</body>
+</html>
