c61ef495104fcac229f26af30f7509513bcac99a
Jonathan Reeve <jon.reeve@gmail.com>
2015-06-22 14:20:07 -0400

    rename 'forums' to 'discussions' in global search

--- lib/mla/bp-global-search.php
+++ lib/mla/bp-global-search.php
@@ -3,6 +3,11 @@
  * Customizations for the BuddyPress Global Search plugin.
  */
 
+/*
+ * Set default search items. This is a reasonable default
+ * so that if the admin forgets to set these searchable items,
+ * bp-global-search will still search roughly the things we want.
+ */
 function mla_filter_bp_global_search_items( $search_items ) {
 	if ( empty( $search_items ) ) {
 		$search_items = array(
@@ -18,3 +23,14 @@ function mla_filter_bp_global_search_items( $search_items ) {
 	return $search_items;
 }
 add_filter( 'buddyboss_global_search_option_items-to-search', 'mla_filter_bp_global_search_items', 5 );
+
+/* Rename 'Forums' to 'Discussions'
+ */
+function mla_rename_forums( $item ) {
+	if ( 'forums' == $item ) {
+		return 'Discussions';
+	} else {
+		return ucfirst( $item ); // Title Case
+	}
+}
+add_filter( 'bboss_global_search_label_search_type', 'mla_rename_forums' );
