26225872cab75f215663ac29558c961123662a4d
Jonathan Reeve <jon.reeve@gmail.com>
2015-07-13 10:37:19 -0400

    adding new sidebar to sidebar area

--- page-dashboard.php
+++ page-dashboard.php
@@ -104,3 +104,5 @@
 <?php auth_redirect(); ?>
 
 <?php endif; ?>
+
+
--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,3 +1,4 @@
 <aside class="sidebar" role="complementary">
 	<?php dynamic_sidebar('sidebar-primary'); ?>
+	<?php dynamic_sidebar('mla-dashboard-tabbed-sidebar'); ?>
 </aside><!-- /.sidebar -->
