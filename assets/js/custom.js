df86c74b58d6652e049172006af61354e23a3708
Ryan Williams <ryrowi@gmail.com>
2016-07-11 09:23:14 -0400

    apply search results tab mismatch workaround to all directory listings

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -115,14 +115,14 @@ jQuery(document).ready(function($) {
    * copy number of results from bottom pagination to overwrite buggy top tab
    */
 
-  var correct_results_el = $('#member-dir-count-bottom');
+  var correct_results_el = $('.dir-list .pag-count');
 
   if (correct_results_el.length) {
     var correct_results_count = (function() {
-      var match = correct_results_el.html().match(/Viewing [0-9]+ - [0-9]+ of ([0-9]+) active members/);
+      var match = correct_results_el.html().match(/Viewing [0-9]+ - [0-9]+ of ([0-9]+)/);
       return (match && match.length > 1) ? match[1] : 0;
     })();
-    var target_el = $('#members-all span');
+    var target_el = $('.main-tabs .selected span');
 
     if (parseInt(correct_results_count) > 0) {
       target_el.html(correct_results_count);
